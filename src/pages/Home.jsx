b61b45f38819b59ef99fffb75ccfb7c2876e4b68
sik2 <bickwang123@gmail.com>
2024-11-11 20:27:38 +0900

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <h1 className="text-3xl font-bold text-center text-blue-600">안녕하세요!</h1>
+            <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">버튼</button>
+        </div>
+    )
+}
+
+export default Home

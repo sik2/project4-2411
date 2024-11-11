19a9ab776ecaae5de65b43662ab58cd68e02af06
sik2 <bickwang123@gmail.com>
2024-11-11 20:32:05 +0900

    set route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
 function App() {
     return (
-        <div className="min-h-screen bg-gray-100">
-            <h1 className="text-3xl font-bold text-center text-blue-600">안녕하세요!</h1>
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">버튼</button>
-        </div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
     )
 }
 

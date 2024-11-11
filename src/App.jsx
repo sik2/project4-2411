8cc3c006d40357d2562565edaf65bf2d39c8b926
sik2 <bickwang123@gmail.com>
2024-11-11 20:51:24 +0900

    add 404 page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 function App() {
     return (
         <Layout>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} /> {/* 404 페이지 추가 */}
             </Routes>
         </Layout>
     )
